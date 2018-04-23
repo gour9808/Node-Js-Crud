63b41293228a1444adb996fe83e4c85867371591
Gourav Sharma <gourav.sharma@widas.in>
2018-04-23 17:24:29 +0530

    added cors

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+});
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 const dbConfig = require('./config/database.config');
@@ -16,16 +20,16 @@ mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose.connect(dbConfig.url)
-.then((res) => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
+    .then((res) => {
+        console.log("Successfully connected to the database");
+    }).catch(err => {
+        console.log('Could not connect to the database. Exiting now...');
+        process.exit();
+    });
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to my Application"});
+    res.json({ "message": "Welcome to my Application" });
 });
 
 // listen for requests
